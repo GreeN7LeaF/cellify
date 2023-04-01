33c9788a3fd60f7e849fec6ae3611f579998a3d0
thaomy <thaomy310702@gmail.com>
2023-04-01 17:56:19 +0700

    fix language and add edit sanpham page

--- Cellphone/Content/assets/js/main.js
+++ Cellphone/Content/assets/js/main.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
     dropDownSideBar();
-    setSideNav();
+    console.log("run into this");
 });
 // DOMContentLoaded  end
+
+setSideNav();
 
 function dropDownSideBar() {
   document.querySelectorAll("#sidebar .nav-link").forEach(function (element) {
@@ -33,36 +35,17 @@ function dropDownSideBar() {
 }
 
 function setSideNav() {
-    document.addEventListener("DOMContentLoaded", function () {
-        document
-            .querySelectorAll("#sidebar .navlink")
-            .forEach(function (element) {
-                element.addEventListener("click", function (e) {
-                    console.log("click");
-                    let nextEl = element.nextElementSibling;
-                    let parentEl = element.parentElement;
+    let route = (window.location.pathname).split("/")[1];
+    /*console.log($("#sidebar").find(`[href="${route}"]`));*/
 
-                    if (nextEl) {
-                        e.preventDefault();
-                        let mycollapse = new bootstrap.Collapse(nextEl);
-
-                        if (nextEl.classList.contains("show")) {
-                            mycollapse.hide();
-                        } else {
-                            mycollapse.show();
-                            // find other submenus with class=show
-                            var opened_submenu =
-                                parentEl.parentElement.querySelector(
-                                    ".submenu.show"
-                                );
-                            // if it exists, then close all of them
-                            if (opened_submenu) {
-                                new bootstrap.Collapse(opened_submenu);
-                            }
-                        }
-                    }
-                }); // addEventListener
-            }); // forEach
+    $item = $('#sidebar .nav-link').filter(function () {
+        return $(this).prop('href').indexOf(route) !== -1;
     });
-    // DOMContentLoaded  end
+
+    $authHeader = $("#auth-header .nav-link").filter(function () {
+        return $(this).prop("href").indexOf(route) !== -1;
+    });
+
+    $item.map((idx, val) => console.log(val.classList.toggle('active')));
+    $authHeader.map((idx, val) => console.log(val.classList.toggle("active")));
 }
